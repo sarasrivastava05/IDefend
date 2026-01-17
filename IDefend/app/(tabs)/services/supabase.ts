d22dd63a783c89f1b3ae805798d7d9058f44575f
sara <ssriva16@ucsc.edu>
2026-01-16 19:17:43 -0800

    supabase fix

---
+++ IDefend/app/(tabs)/services/supabase.ts
@@ -0,0 +1,15 @@
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'YOUR_SUPABASE_URL'; // From step 1
+const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY'; // From step 1
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
